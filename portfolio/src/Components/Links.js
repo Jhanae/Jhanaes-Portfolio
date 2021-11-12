105bf948da4dc837f70739dad5d90ad0cfc03a99
Your name <you@example.com>
2021-11-12 18:54:31 -0500

    Added new components

--- portfolio/src/Components/Links.js
+++ portfolio/src/Components/Links.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import clipboard from "../Pictures/clipboard.png";
 
 function Links() {
     return (
         <div>
             <div className="Linksbackground">
-            <h1>Projects</h1>
-                <h3>I am sjfdjnjdsnc <br/> hfgzfjgvfjvjfbvjfb</h3>
+                {/* <img src={clipboard} alt="clip"/> */}
+                <div>
+                    <h1>Projects</h1>
+                    <h3>I am sjfdjnjdsnc <br/> hfgzfjgvfjvjfbvjfb</h3>
+                </div>
             </div>
         </div>
     )
